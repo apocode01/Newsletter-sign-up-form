717b6ac44e6eaad819df1039821f85c8a662ec85
apocode01 <apopanou@gmail.com>
2025-08-22 10:52:34 +0300

    tablet and more functionality

--- index.js
+++ index.js
@@ -1,17 +1,33 @@
 const form = document.querySelector('.form');
+const field = form.querySelector('#name');
+const formState = document.querySelector('.formState');
+const successState = document.querySelector('.successState');
+const errorMessage = form.querySelector('#name-error');
 
 form.addEventListener('submit', event => {
     event.preventDefault();
 
-    const field = event.target.querySelector('#name');
     const email = field.value;
 
     const regex = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/i;
     if (regex.test(email)) {
-
+        formState.style.display = "none";
+        successState.style.display = "flex";
+        const emailSpan = document.querySelector('.bold');
+        emailSpan.textContent = email;
     }
     else {
         field.classList.add('nameError');
-        console.log(field);
+        errorMessage.textContent = "Valid email required";
     }
+})
+
+const dismissButton = document.getElementById('dismiss');
+
+dismissButton.addEventListener('click', event => {
+    field.value = "";
+    field.classList.remove('nameError');
+    errorMessage.textContent = "";
+    formState.style.display = "flex";
+    successState.style.display = "none";
 })
